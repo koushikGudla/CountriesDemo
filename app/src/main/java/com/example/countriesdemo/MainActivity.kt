c89f3dc532402e733f21da45896163dfcb6ddcd4
koushik_g <koushik_g@outlook.com>
2025-03-13 05:38:02 -0500

    updated MainActivity code to use the stateFlow exposed by the ViewModel and also updated the UI

--- app/src/main/java/com/example/countriesdemo/MainActivity.kt
+++ app/src/main/java/com/example/countriesdemo/MainActivity.kt
@@ -2,19 +2,54 @@ package com.example.countriesdemo
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.example.countriesdemo.databinding.ActivityMainBinding
+import com.example.countriesdemo.presentation.list.CountriesAdapter
+import com.example.countriesdemo.presentation.list.CountryListViewModel
+import com.example.countriesdemo.presentation.viewModelFactory
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            ViewCompat.setOnApplyWindowInsetsListener(main) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+
+            val countriesAdapter by lazy { CountriesAdapter() }
+            val viewModel: CountryListViewModel by viewModels {
+                viewModelFactory { CountryListViewModel(CountryApp.appModule.countryRepo) }
+            }
+
+            countriesList.adapter = countriesAdapter
+
+            viewModel.state.flowWithLifecycle(lifecycle)
+                .onEach { viewState ->
+                    loadingIndicator.isVisible = viewState.isLoading
+                    countriesList.isVisible = viewState.countries.isNotEmpty()
+                    countriesAdapter.submitList(viewState.countries)
+                    errorText.isVisible = viewState.error.isNotBlank()
+                    errorText.text = viewState.error
+                }.launchIn(lifecycleScope)
+
+        }.also { setContentView(it.root) }
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
